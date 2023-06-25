53058adafb306fe138d8007dbe73e27c1aad7f71
tufan özkan <tufanozkan1905@outlook.com>
2023-06-25 18:13:37 +0300

    newgame kaldı

--- jvscrpt/jvscrpt.js
+++ jvscrpt/jvscrpt.js
@@ -1,16 +1,25 @@
 let at=document.getElementById("throw");
 let plyzar= document.querySelector(".plyzar");
 let cmpzar= document.querySelector(".cmpzar");
+let plyimg = document.createElement("img");
+let cmpimg = document.createElement("img");
 
+let plsay=0;
+let cmpsay=0;
 
 at.addEventListener('click',game);
 
 function game(e){
 
+    plyimg.remove();
+    cmpimg.remove();
+
+    let pls= document.querySelector("#plscore");
+    let cmps= document.querySelector("#cmpscore");
+
     let plydizi= [1,2,3,4,5,6];
     let plyrastind = Math.floor(Math.random() * plydizi.length);
 	let plyrast=plydizi[plyrastind];
-    
 
     let cmpdizi= new Array(1,2,3,4,5,6);
     let cmprastind = Math.floor(Math.random() * cmpdizi.length);
@@ -18,91 +27,127 @@ function game(e){
 
     console.log(plyrast,cmprast)
 
+    
     if (plyrast==1) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/1.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        plyzar.appendChild(imageElement);
         
-    } else if(plyrast==2) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/2.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        plyzar.appendChild(imageElement);
+        plyimg.src = "./stil/1.jpg";
+        plyimg.style.width="100px";
+        plyimg.style.height="100px";
+        plyzar.appendChild(plyimg);
+
+        plsay+=plyrast;
+        pls.textContent=plsay;
+    } 
+    else if(plyrast==2) {
+        
+        plyimg.src = "./stil/2.jpg";
+        plyimg.style.width="100px";
+        plyimg.style.height="100px";
+        plyzar.appendChild(plyimg);
+
+        plsay+=plyrast;
+        pls.textContent=plsay;
     }
     else if(plyrast==3) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/3.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        plyzar.appendChild(imageElement);
+        
+        plyimg.src = "./stil/3.jpg";
+        plyimg.style.width="100px";
+        plyimg.style.height="100px";
+        plyzar.appendChild(plyimg);
+
+        plsay+=plyrast;
+        pls.textContent=plsay;
     }
     else if(plyrast==4) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/4.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        plyzar.appendChild(imageElement);
+        
+        plyimg.src = "./stil/4.jpg";
+        plyimg.style.width="100px";
+        plyimg.style.height="100px";
+        plyzar.appendChild(plyimg);
+
+        plsay+=plyrast;
+        pls.textContent=plsay;
     }
     else if(plyrast==5) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/5.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        plyzar.appendChild(imageElement);
+        
+        plyimg.src = "./stil/5.jpg";
+        plyimg.style.width="100px";
+        plyimg.style.height="100px";
+        plyzar.appendChild(plyimg);
+        
+        plsay+=plyrast;
+        pls.textContent=plsay;
     }
     else if(plyrast==6) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/6.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        plyzar.appendChild(imageElement);
+        
+        plyimg.src = "./stil/6.jpg";
+        plyimg.style.width="100px";
+        plyimg.style.height="100px";
+        plyzar.appendChild(plyimg);
+
+        plsay+=plyrast;
+        pls.textContent=plsay;
     }
 
 
     if (cmprast==1) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/1.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        cmpzar.appendChild(imageElement);
         
-    } else if(cmprast==2) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/2.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        cmpzar.appendChild(imageElement);
+        cmpimg.src = "./stil/1.jpg";
+        cmpimg.style.width="100px";
+        cmpimg.style.height="100px";
+        cmpzar.appendChild(cmpimg);
+
+        cmpsay+=cmprast;
+        cmps.textContent=cmpsay;
+    } 
+    else if(cmprast==2) {
+        
+        cmpimg.src = "./stil/2.jpg";
+        cmpimg.style.width="100px";
+        cmpimg.style.height="100px";
+        cmpzar.appendChild(cmpimg);
+
+        cmpsay+=cmprast;
+        cmps.textContent=cmpsay;
     }
     else if(cmprast==3) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/3.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        cmpzar.appendChild(imageElement);
+        
+        cmpimg.src = "./stil/3.jpg";
+        cmpimg.style.width="100px";
+        cmpimg.style.height="100px";
+        cmpzar.appendChild(cmpimg);
+
+        cmpsay+=cmprast;
+        cmps.textContent=cmpsay;
     }
     else if(cmprast==4) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/4.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        cmpzar.appendChild(imageElement);
+        
+        cmpimg.src = "./stil/4.jpg";
+        cmpimg.style.width="100px";
+        cmpimg.style.height="100px";
+        cmpzar.appendChild(cmpimg);
+
+        cmpsay+=cmprast;
+        cmps.textContent=cmpsay;
     }
     else if(cmprast==5) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/5.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        cmpzar.appendChild(imageElement);
+        
+        cmpimg.src = "./stil/5.jpg";
+        cmpimg.style.width="100px";
+        cmpimg.style.height="100px";
+        cmpzar.appendChild(cmpimg);
+
+        cmpsay+=cmprast;
+        cmps.textContent=cmpsay;
     }
     else if(cmprast==6) {
-        let imageElement = document.createElement("img");
-        imageElement.src = "./stil/6.jpg";
-        imageElement.style.width="100px";
-        imageElement.style.height="100px";
-        cmpzar.appendChild(imageElement);
-    }
+        
+        cmpimg.src = "./stil/6.jpg";
+        cmpimg.style.width="100px";
+        cmpimg.style.height="100px";
+        cmpzar.appendChild(cmpimg);
 
+        cmpsay+=cmprast;
+        cmps.textContent=cmpsay;
+    }
 }
